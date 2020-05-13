b8d5b6744d248ff1958d4f8a9cd12e8a309fce3c
Ben Parish <parishben13@gmail.com>
2020-05-13 03:37:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,26 @@ def monopoly_with_second_tier
     :pieces=>4,
     :names=>{}
   :rent_in_dollars=> {}
-=begin    {
+  
+    }
+  }
+}
+=end
+ }
+}
+end
+
+def monopoly_with_third_tier
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
+end
+
+def monopoly_with_fourth_tier
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+
+end
+=begin
+=begin {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
@@ -77,20 +96,4 @@ def monopoly_with_second_tier
       {
         "mortgage_value"=>"$800"
       }
-    }
-  }
-}
-=end
- }
-}
-end
-
-def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
-end
-
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
-end
+    =end
