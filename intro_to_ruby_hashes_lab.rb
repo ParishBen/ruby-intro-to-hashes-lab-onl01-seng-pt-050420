e34df7c2a67493be8d4ee63b4e70390d5fd840e8
Ben Parish <parishben13@gmail.com>
2020-05-13 03:31:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_second_tier
 	    :railroads=>
  {
     :pieces=>4,
-    :rent_in_dollars=>
+=begin   :rent_in_dollars=>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
